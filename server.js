d423c3b074e183843435fb2a8f1a6770517241a0
jnlar <jon357@protonmail.ch>
2021-05-30 19:00:01 +1000

    no redis caching!

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const { 
-	express, fetch, redis, rateLimit, socket,
+	express, fetch, redis, rateLimit,
 	limiter, PORT, client, app, server 
 } = require('./modules');
 
-const io = socket(server)
 app.use(express.urlencoded({ extended: true }))
 
 const cache = (req, res, next) => {
@@ -13,7 +12,8 @@ const cache = (req, res, next) => {
 		if (err) throw err;
 
 		if (data !== null) {
-			res.send(setResponse(username, data));
+			res.json(data)
+			//res.send(setResponse(username, data));
 		} else next();
 	})
 }
@@ -21,48 +21,28 @@ const cache = (req, res, next) => {
 const getRepos = async (req, res, next) => {
 	try {
 		console.log('Fetching...')
-		const { username } = req.params;
+		//const { username } = req.params;
+		const username = req.query.username;
 		const response = await fetch(`https://api.github.com/users/${username}`);
 
 		const data = await response.json();
 		const repos = data.public_repos;
 
 		// clear cache after 10 minutes
-		// TODO: LRU cache implementation
-		client.setex(username, 600, repos);
+		// TODO: LRU cache implementation - https://redis.io/topics/lru-cache
+		//client.setex(username, 600, repos);
 
-		res.send(setResponse(username, repos));
+		//res.send(setResponse(username, repos));
+		res.json(data)
+		console.log(data)
+		res.end()
 	} catch (err) {
 		console.error(err);
 	}
 }
 
-//const getRepos = async (req, res, next) => {
-//	try {
-//		console.log('Fetching...');
-//		const username = data.username;
-//		const response = await fetch(`https://api.github.com/users/${username}`);
-//
-//		const data = await response.json();
-//		const repos = data.public_repos;
-//
-//		client.setex(username, 600, repos);
-//		return setResponse(username, repos);
-//
-//	} catch (err) {
-//		console.error(err);
-//	}
-//}
-
 const setResponse = (username, repos) => {
 	return `<p>${username} has ${repos} Github repositories!</p>`;
 }
 
-app.get('/repos/:username', limiter, cache, getRepos);
-
-io.on('connection', socket => {
-	socket.on('get-user', data => {
-
-		io.emit('get-user', data.username);
-	})
-})
+app.get('/get', limiter, /*cache,*/ getRepos);
